03ef7bd39fa3dfdcd4996f7fcfda2fd761273b2c
ayushd150 <ayushdbpc2005@gmail.com>
2025-08-15 00:45:52 +0530

    made new changes

--- w23/backend/src/index.ts
+++ w23/backend/src/index.ts
@@ -1,10 +1,30 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port:8080 });
+
+let senderSocket: null | WebSocket = null;
+let recieverSocket: null | WebSocket = null;
 
 wss.on('connection', function onnection(ws){
     ws.on('message', function message(data: any){
         const message = JSON.parse(data);
+        if (message.type === "identify-as-sender"){
+            senderSocket = ws;
+        }
+        else if (message.type === "identify-as-receiver"){
+            recieverSocket = ws
+        }
+        else if (message.type === "create-offer"){
+            recieverSocket?.send(JSON.stringify({type:"offer", offer:message.offer}))
+        }
+        else if (message.type === "create-answer"){
+            senderSocket?.send(JSON.stringify({type: "offer", offer: message.offer}))
+        } 
+        // The ?. checks if recieverSocket exists before calling .send().
+// If recieverSocket is null or undefined, it won’t try to call .send(), preventing a runtime error.
+
+// This is safer than writing recieverSocket.send(...) directly, which would crash if recieverSocket is not set.
+
         // create offer msg
         // create offer msg
         // create offer msg
